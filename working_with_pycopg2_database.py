4901aa92b0d3298e56592fc82345f265a559c351
thomasdinh2k <dnstung@icloud.com>
2023-04-27 10:37:58 +0700

    Update "Delete_item()"

--- working_with_pycopg2_database.py
+++ working_with_pycopg2_database.py
@@ -5,6 +5,11 @@ global table_name
 table_name = ''
 
 
+def separator():
+    print("*" * 50)
+    print('\n' * 1)
+
+
 def choosing_table():       # Choose which table to execute actions
     connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
     cursor = connection.cursor()
@@ -16,7 +21,7 @@ def choosing_table():       # Choose which table to execute actions
     return table_option
 
 
-def ask_for_insert():       # Ask user to enough input to make a table in database
+def ask_for_insert():       # Ask user for data to store in table
     var_num = pa.inputInt(prompt="How many variable do you want? ", min=1)
     column_names = []
     result_string = {}
@@ -39,10 +44,11 @@ def ask_for_insert():       # Ask user to enough input to make a table in databa
     return final_result_text
 
 
-def view():
-    global table_name
-    if table_name == '':
+def view(table_name=None):
+    if table_name == None:
         table_name = choosing_table()
+    else:
+        pass
     connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM {table_name}")   # Obtain Data from the database
@@ -51,6 +57,7 @@ def view():
     print(f"\nTable {table_name.upper()} contains:\n ")
     for row in rows:
         print(row, end="\n")
+    separator()
     return table_name
 
 
@@ -59,41 +66,97 @@ def create_table():
     cursor = connection.cursor()
     table_name = pa.inputStr(prompt="What is the name of this new table?\n")
     execute_line = ask_for_insert()
-    print(f"CREATE TABLE IF NOT EXISTS {table_name} ({execute_line})")
     cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({execute_line})")
     connection.commit()
     cursor.close()
     connection.close()
-    print(f"TABLE {table_name} HAS BEEN CREATED SUCCESSFULLY")
+    print(f"TABLE [{table_name}] HAS BEEN CREATED SUCCESSFULLY")
 
 
 def insert_item():
-    connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
-    cursor = connection.cursor()
-    global table_name
-    if table_name == '':
-        table_name = choosing_table()
-    if table_name == "store":
-        item = pa.inputStr(prompt="Which item is this? ")
-        quantity = pa.inputInt(prompt="How many item? ")
-        price = pa.inputInt(prompt="What's the price then? ")
-    cursor.execute(f"INSERT INTO {table_name} VALUES (%s,%s,%s)", (item, quantity, price))
-    print("ITEM ({}) INSERTED SUCCESSFULLY".format(item.upper()))
-    view()
-    return table_name
-
-
-def delete_item(item):
-    connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
-    cursor = connection.cursor()
     table_name = choosing_table()
-    cursor.execute(f"DELETE FROM {table_name} WHERE item=%s", (item,))       # Must have "," after item if this have one item only
+    connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
+    cursor = connection.cursor()
+    cursor.execute(f'''
+        SELECT column_name, data_type
+        FROM information_schema.columns
+        WHERE table_name = '{table_name}' 
+    ''')        # Get column_name and column_type data
+    table_property = cursor.fetchall()
+    print(table_property)
+    table_column_name = []
+    adding_value = []
+    for i in range(len(table_property)):
+        table_column_name.append(table_property[i][0])
+        adding_value_text = input((f"Type input for column [{table_property[i][0]}]:\n"))
+        if adding_value_text.isalpha():
+            adding_value_text = f"\'{adding_value_text}\'"
+            adding_value.append(adding_value_text)
+        else:
+            adding_value.append(adding_value_text)
+    for value in table_column_name, adding_value:
+        column_name_string = ', '.join(table_column_name)
+        # adding_value_string = ', '.join(adding_value)
+        adding_value_string = ', '.join(adding_value)
+
+    print(f"INSERT INTO {table_name} ({column_name_string}) VALUES ({adding_value_string})")
+    cursor.execute(f"INSERT INTO {table_name} ({column_name_string}) VALUES ({adding_value_string})")
     connection.commit()
+    print(f"Value {adding_value} added into {table_name} successfully!")
+    view(table_name)
+    cursor.close()
     connection.close()
-    print("ITEM ({}) DELETED SUCCESSFULLY".format(item.upper()))
+
+
+def delete_item():
+    # DELETE FROM links WHERE id = 8
+    connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
+    cursor = connection.cursor()
+    flag = True
+    while flag:
+        table_name = choosing_table()
+        cursor.execute(f'''
+                SELECT column_name, data_type
+                FROM information_schema.columns
+                WHERE table_name = '{table_name}' 
+            ''')  # Get column_name and column_type data
+        table_property = cursor.fetchall()
+        print(table_property)
+        table_column_name = []      # An empty string to pass data in
+        table_column_value = []
+        for i in range(len(table_property)):
+            table_column_name.append(table_property[i][0])
+            table_column_value.append(table_property[i][1])
+        column_selection = pa.inputMenu(table_column_name, prompt='Which column?\n', numbered=True)
+        print(table_column_name)
+
+        # View all data in "{column_selection} column
+        cursor.execute(f"SELECT {column_selection} FROM {table_name}")
+        result = cursor.fetchall()
+        # Test print the result
+        data_bank = []
+        for data_row in result:
+            if data_row[0] not in data_bank:
+                data_bank.append(data_row[0])
+            else:
+                pass
+        data_bank_fix = [str(item) for item in data_bank]
+        # Test if result aligned
+        print(data_bank)
+        data_selection = pa.inputMenu(data_bank_fix, numbered=True)
+        confirm = f"DELETE FROM {table_name} WHERE {column_selection}={data_selection}"
+        print(confirm)
+        if pa.inputMenu(['yes', 'no'], prompt='Are you sure you want to delete\n', numbered=True) == 'yes':
+            cursor.execute(confirm)
+            connection.commit()
+            connection.close()
+            cursor.close()
+        else:
+            flag = False
 
 
 def update_item():
+    # UPDATE {table_name} SET {table_column_name} = {new_input} WHERE {table_property} = {} AND {} = {}
     connection = psycopg2.connect("dbname='database1' user='postgres' password='1820' host='localhost' port='5432'")
     cursor = connection.cursor()
     global table_name
@@ -106,10 +169,24 @@ def update_item():
         cursor.execute(f"UPDATE {table_name} SET quantity=%s, price=%s WHERE item=%s", (quantity, price, item))       # Must have , after item
         print("ITEM ({}) UPDATED SUCCESSFULLY".format(item.upper()))
     else:
-        print('THIS FUNCTION IS CURRENTLY UNDER DEVELOPMENT')
+        set = input("SET? ")
+        cursor.execute(f"Update {table_name} SET {set}")
     connection.commit()
     connection.close()
     return table_name
 
-
-view()
+print("Welcome to Database Pycopg2 Tool")
+database_action = ['View', 'Insert', 'Update', 'Delete', 'Quit']
+while True:
+    user_choice = pa.inputMenu(database_action, prompt="What do you want to do?\n", numbered=True)
+    if user_choice == 'View':
+        view()
+    elif user_choice == 'Insert':
+        insert_item()
+    elif user_choice == 'Update':
+        update_item()
+    elif user_choice == 'Delete':
+        delete_item()
+    else:
+        break
+print("Goodbye")

71402bb4446fbd571fbf5e1a5b1e735515aaaf6c
thomasdinh2k <work.dnstung@gmail.com>
2023-08-02 10:50:32 +0700

    Lesson 09

--- JAVASCRIPT-COURSE/08-object.js
+++ JAVASCRIPT-COURSE/08-object.js
@@ -12,3 +12,67 @@ product.newProperty = true;
 delete product.newProperty; //Delete a property
 console.log(product)
 updateResult(product.name, "demo");
+
+//Instrument Sound
+const trumpet = {
+    name: "Trumpet",
+    type: "Brass",
+    sound: "🎺 Tu-tu-tu-tu! 🎺",
+};
+
+const saxophone = {
+    name: "Saxophone",
+    type: "Woodwind",
+    sound: "🎷 Bee-boo-bop! 🎷",
+};
+
+const trombone = {
+    name: "Trombone",
+    type: "Brass",
+    sound: "🎶 Wa-wa-wa-wa! 🎶",
+};
+
+
+const violin = {
+    name: "Violin",
+    type: "String",
+    sound: "🎻 Screeeech! 🎻",
+  };
+  
+const clarinet = {
+    name: "Clarinet",
+    type: "Woodwind",
+    sound: "🎼 Doo-doo-doo! 🎼",
+  };
+  
+const flute = {
+    name: "Flute",
+    type: "Woodwind",
+    sound: "💨 Toooooot! 💨",
+  };
+  
+  const guitar = {
+    name: "Guitar",
+    type: "String",
+    sound: "🎸 Strum-strum-strum! 🎸",
+  };
+  
+  const drum = {
+    name: "Drum",
+    type: "Percussion",
+    sound: "🥁 Boom-boom-tss! 🥁",
+  };
+  
+  const piano = {
+    name: "Piano",
+    type: "Keyboard",
+    sound: "🎹 Ding-ding-ding! 🎹",
+  };
+
+
+function play_sound(instrument) {
+    updateResult(instrument.sound, 'Instrument')
+}
+
+
+

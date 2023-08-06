a3237574eb6044c33de0416c9df9ca0919ff3551
thomasdinh2k <work.dnstung@gmail.com>
2023-08-06 08:46:10 +0700

    Commit

--- JAVASCRIPT-COURSE/Scripts/11x.js
+++ JAVASCRIPT-COURSE/Scripts/11x.js
@@ -1,20 +1,43 @@
-// let todoList = [
-//     'wash dishes',
-//     'make dinner',
-//     'clean toilet'
-// ];
+const todoList = [{
+    name: 'wash dishes',
+    due: '2022-12-22'},
+    {
+        name : 'make dinner',
+        due: '2023-25-10'
+    }, {
+        name: 'clean the toilet',
+        due: '12-12-2012'
+    }    
+];
 
-let todoList = JSON.parse(localStorage.getItem("todoList"));
+// let todoList = JSON.parse(localStorage.getItem("todoList"));
+
+console.log(todoList)
 
 function renderHTML(list) {
     let todoListHTML = '';
     for (let i = 0; i < list.length ; i ++) {
-        const todo = list[i];
-        console.log(todo);
-        const HTML = `<p>${todo}</p>`
-        todoListHTML += HTML;
-        console.log(todoListHTML);
+        const todoObject = list[i];
+        if (todoObject !== '') {
+            console.log(todoObject);
+            const HTML = `<p>
+                ${todoObject} 
+                <button onclick="
+                    deleteTodo(${i});
+                ">DELETE</button>
+            </p>`
+            todoListHTML += HTML;
+        }
         document.querySelector('.listOfTodo').innerHTML = todoListHTML;
+    }
+}
+
+function deleteTodo(index) {
+    if (index <= 1 && todoList.length <= 1) {
+        document.querySelector('.listOfTodo').innerHTML = '';  // Make sure the last element is deleted
+    } else {
+        todoList.splice(index, 1);
+        renderHTML(todoList);
     }
 }
 
@@ -35,7 +58,6 @@ function viewALL(){
 }
 
 function handleKeyDown(event) {
-    console.log(event.key)
     if(event.key === 'Enter'){
         todoAction();
     } else if (event.key === 'Meta') {

23d940f33e2a159bece6aac853ee8545cda9cbcf
thomasdinh2k <work.dnstung@gmail.com>
2023-08-23 10:28:45 +0700

    Added formatCurrency func to share bwt scripts

--- JAVASCRIPT-COURSE/Amazon-Website/scripts/amazon.js
+++ JAVASCRIPT-COURSE/Amazon-Website/scripts/amazon.js
@@ -1,5 +1,6 @@
 import { products } from "../data/products.js";
 import { cart, addToCart } from "../data/cart.js";
+import { formatCurrency } from "./utils/money.js";
 let productsHTML = "";
 
 products.forEach((products) => {
@@ -23,7 +24,7 @@ products.forEach((products) => {
     </div>
 
     <div class="product-price">
-        $${(products.priceCents / 100).toFixed(2)}
+        $${formatCurrency(products.priceCents)}
     </div>
 
     <div class="product-quantity-container">

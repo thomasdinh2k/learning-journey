9b8cb2c6d3575730a28ee89f51df8295be5df07c
thomasdinh2k <work.dnstung@gmail.com>
2023-09-25 05:18:21 +0700

    Complete Calculator

--- JAVASCRIPT-COURSE/Calculator App/script.js
+++ JAVASCRIPT-COURSE/Calculator App/script.js
@@ -2,94 +2,93 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-const screen = document.querySelector('.screen');
+const screen = document.querySelector(".screen");
 
-function buttonClick(value){
-    if(isNaN(value)) {
-        handleSymbol(value);
-    } else{
-        handleNumber(value);
-    }
-    screen.innerText = buffer;
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
 }
 
-function handleSymbol(symbol){
-    switch(symbol){
-        case 'C':
-            buffer = '0';
-            runningTotal = 0;
-            break;
-        case '=':
-            if(previousOperator === null) {
-                return 
-            }
-            flushOperation(parseInt(buffer));
-            previousOperator = null;
-            buffer = runningTotal;
-            runningTotal = 0;
-            break;
-        case '←':
-            if(buffer.length === 1){
-                buffer = '0';
-
-            } else {
-                buffer = buffer.substring(0, buffer.length - 1);
-
-            }
-            break;
-        case '+':
-        case '−':
-        case 'x':
-        case '÷':
-            handleMath(symbol);
-            break;
-        
-    }
-}
-
-function handleMath(symbol){
-    if(buffer === '0') {
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
         return;
-    }
-
-    const intBuffer = parseInt(buffer);
-
-    if (runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperation(intBuffer);
-    }
-    previousOperator = symbol;
-    buffer = '0';
+      }
+      flushOperation(parseInt(buffer));
+      previousOperator = null;
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "x":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
 }
 
-function flushOperation(intBuffer){
-    if(previousOperator === '+') {
-        runningTotal += intBuffer;
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
 
-    }else if(previousOperator === '-'){
-        runningTotal -= intBuffer;
-    }else if(previousOperator === 'x'){
-        runningTotal *= intBuffer;
-    }else if(previousOperator === '+'){
-        runningTotal /= intBuffer;
-    }
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = "0";
 }
 
-function handleNumber(numberString){
-    if(buffer === "0"){
-        buffer = numberString;
-    } else {
-        buffer += numberString;
-    }
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "x") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
 }
 
-function init(){
-    document.querySelector('.calc-buttons').
-    addEventListener('click', function(event){
-        buttonClick(event.target.innerText);
-    })
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    console.log(`Buffer now = ${buffer}`);
+    buffer = numberString;
+  } else {
+    buffer += numberString; // Update more digit to the screen when user put more digits
+    console.log(`Buffer now = ${buffer}`);
+  }
+}
 
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+      console.log(event.target.innerText);
+    });
 }
 
 init();

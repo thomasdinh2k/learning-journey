cc2675bb9caea01d70ecf892cd296d0a0d103085
thomasdinh2k <work.dnstung@gmail.com>
2023-11-22 10:35:25 +0700

    New project commit

---
+++ REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Comming Soon...</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut unde, deserunt sapiente magni impedit iure rem reiciendis libero velit est facilis laboriosam eaque, amet repellendus dolore molestiae recusandae optio sit quisquam dolorum sed? Laudantium iusto ad voluptatem voluptatibus mollitia veniam!</p>
+    </>
+  )
+}
+
+export default App

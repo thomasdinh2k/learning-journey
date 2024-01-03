ebf22a05d06c45583eabf4c60ddd7969fc104cce
thomasdinh2k <work.dnstung@gmail.com>
2024-01-03 16:47:12 +0700

    Fix bug: Can't change player name

--- REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/components/Player.jsx
+++ REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/components/Player.jsx
@@ -2,55 +2,44 @@ import { useState } from "react";
 
 const Player = ({ initialName, playerSymbol, isPlayerActive }) => {
 	const [inputState, changeInputState] = useState(false);
-	const [firstTimeChange, setFirstTimeChange] = useState(true);
+
 	const [playerName, setPlayerName] = useState(initialName);
 
+	function handleEditClick() {
+		changeInputState(!inputState);
+	}
 
-	const handleEditClick = () => {}
+	function handleNameChange(event) {
+		setPlayerName(event.target.value);
+	}
 
-
-	const showPlayerNameInput = () => {
-		// changeInputState(!inputState);
-		changeInputState(() => !inputState);
-	};
-
-	const handleNameChange = (event) => {
-		const outputName = event.target.value;
-		setPlayerName(outputName);
-		setFirstTimeChange(false);
-	};
-
-	const updatePlayerName = () => {
-		// handleNameChange();
-		changeInputState(false);
-	};
-
-	const handleEnter = (event) => {
+	function handleEnter(event) {
 		if (event.key === "Enter") {
-			updatePlayerName();
+			handleEditClick();
 		}
-	};
-
+	}
 	const editablePlayerName = inputState ? (
 		<input
 			className="player-name player"
-			value={firstTimeChange ? " " : playerName}
+			// value={playerName}
 			type="text"
 			required
 			onChange={handleNameChange}
-			onBlur={updatePlayerName}
-			onKeyDown={(event) => {
-				handleEnter(event);
-			}}
+			onBlur={handleEditClick}
+			onKeyDown={handleEnter}
 		></input>
-	) : (<span className="player-name">{playerName}</span>);
+	) : (
+		<span className="player-name">{playerName}</span>
+	);
 	return (
 		<li className={isPlayerActive === playerSymbol ? "active" : undefined}>
 			<span className="player">
 				{editablePlayerName}
 				<span className="player-symbol">{playerSymbol}</span>
 			</span>
-			<button onClick={handleEditClick}>{inputState ? "Save" : "Modify"}</button>
+			<button onClick={handleEditClick}>
+				{inputState ? "Save" : "Modify"}
+			</button>
 		</li>
 	);
 };

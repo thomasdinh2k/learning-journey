e23babc5bf8aa622b956a953bdf06cab01bb67c5
thomasdinh2k <work.dnstung@gmail.com>
2024-01-06 19:08:46 +0700

    add scss

--- NODE-COURSE/blog/src/index.js
+++ NODE-COURSE/blog/src/index.js
@@ -6,25 +6,29 @@ const { engine, create, ExpressHandlebars } = require("express-handlebars");
 
 const app = express();
 const port = 3000;
-const path = require('path');
+const path = require("path");
 
 // handlebars - Template engine
-app.engine("hbs", engine( {extname: '.hbs'} ));
+app.engine("hbs", engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, 'resources/views'));
+app.set("views", path.join(__dirname, "resources/views"));
+
+// Add static images
+// app.use("/static", express.static(`${__dirname}/../public/`));
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, "public"));
+
 
 // HTTP Logger
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
+app.get("/", (req, res) => {
+	res.render("home");
+});
+app.get("/news", (req, res) => {
+	res.render("news");
+});
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-app.get('/news', (req, res) => {
-    res.render('news');
-})
-
-
-app.listen( port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
 });

8a8d6c5abaded81c493e809e791f0066adabe283
thomasdinh2k <work.dnstung@gmail.com>
2024-01-15 17:43:52 +0700

    initial commit

--- REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/components/GameBoard.jsx
+++ REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/components/GameBoard.jsx
@@ -50,7 +50,7 @@ const GameBoard = ({ onSelectSquare, gameTurns }) => {
 							<ol>
 								{row.map((playerSymbol, colIndex) => (
 									<li key={colIndex}>
-										<button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+										<button disabled={playerSymbol != null} onClick={() => onSelectSquare(rowIndex, colIndex)}>
 											{playerSymbol}
 										</button>
 									</li>

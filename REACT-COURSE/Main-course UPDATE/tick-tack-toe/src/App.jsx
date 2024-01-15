4b4e362f8c1f89af9da3128dcdc7ba4a365dfc02
thomasdinh2k <work.dnstung@gmail.com>
2024-01-15 18:42:31 +0700

    additional commit

--- REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/App.jsx
+++ REACT-COURSE/Main-course UPDATE/tick-tack-toe/src/App.jsx
@@ -7,7 +7,9 @@ import { WINNING_COMBINATIONS } from "./assets/winning-combinations";
 
 const turnDecider = (gameTurns) => {
 	let currentPlayer = "X";
-	
+	console.log("gameTurns", gameTurns);
+
+
 	// prevTurns[0] là turn gần đây nhất được đưa vào State, giờ mình lấy ra để decide turn
 	if (gameTurns.length > 0 && gameTurns[0].player === "X") {
 		currentPlayer = "O";
@@ -18,10 +20,15 @@ const turnDecider = (gameTurns) => {
 	return currentPlayer;
 };
 
+
 function App() {
 	const [gameTurns, setGameTurns] = useState([]);
 	let activePlayer = turnDecider(gameTurns);
 	
+	function decideWinner(gameTurns) {
+		console.log("WinnerDATA", gameTurns);
+	}
+
 	function handleSelectSquare(rowIndex, colIndex) {
 		setGameTurns((prevTurns) => {
 			// let currentPlayer = "X";
@@ -36,62 +43,15 @@ function App() {
 				...prevTurns,
 			];
 			console.log("updatedTurns", updatedTurns);
+			
+			decideWinner(updatedTurns)
 			return updatedTurns;
 		});
 
-		// [
-		// 	{
-		// 		"square": {
-		// 			"row": 0,
-		// 			"col": 1
-		// 		},
-		// 		"player": "O"
-		// 	},
-		// 	{
-		// 		"square": {
-		// 			"row": 0,
-		// 			"col": 0
-		// 		},
-		// 		"player": "X"
-		// 	},
-		// 	{
-		// 		"square": {
-		// 			"row": 1,
-		// 			"col": 0
-		// 		},
-		// 		"player": "O"
-		// 	},
-		// 	{
-		// 		"square": {
-		// 			"row": 1,
-		// 			"col": 1
-		// 		},
-		// 		"player": "X"
-		// 	},
-		// 	{
-		// 		"square": {
-		// 			"row": 0,
-		// 			"col": 2
-		// 		},
-		// 		"player": "O"
-		// 	},
-		// 	{
-		// 		"square": {
-		// 			"row": 1,
-		// 			"col": 2
-		// 		},
-		// 		"player": "X"
-		// 	}
-		// ]
-
-		// Debug
-
-		/**
-		 * TODO: Đẩy updatedTurns ra Component Log
-		 * Đã sync được history rồi, đẩy ra rồi xem
-		 */
+		
 	}
 
+	
 	return (
 		<main>
 			<div id="game-container">

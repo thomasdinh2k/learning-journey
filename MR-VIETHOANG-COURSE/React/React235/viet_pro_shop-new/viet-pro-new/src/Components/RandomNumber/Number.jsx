8612e4a35e4167dee63a7754bb748219774a2bb7
thomasdinh2k <work.dnstung@gmail.com>
2024-01-26 03:13:45 +0700

    additional commits

--- MR-VIETHOANG-COURSE/React/React235/viet_pro_shop-new/viet-pro-new/src/Components/RandomNumber/Number.jsx
+++ MR-VIETHOANG-COURSE/React/React235/viet_pro_shop-new/viet-pro-new/src/Components/RandomNumber/Number.jsx
@@ -2,27 +2,33 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
 const Number = () => {
-	
-    const number = useSelector( (store) => {
-        console.log(store);
-        return store.num
-    })
+	const number = useSelector((store) => {
+		console.log(store);
+		return store.num;
+	});
 
-    const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-    const handleGetNumber = (reset) => {
-        if (reset == "reset") {
-            dispatch({
-                type: "GET_RESET"
-            })
-        } else {
-            dispatch({
-                type: "GET_NUMBER"
-            })
-        }
-    }
+	const handleGetNumber = (reset) => {
+		if (reset == "reset") {
+			dispatch({
+				type: "GET_RESET",
+			});
+		} else if (reset == "love") {
+			dispatch({
+                type: "GET_LOVE",
+                payload: {
+                    data: 3000
+                }
+            });
+		} else {
+			dispatch({
+				type: "GET_NUMBER",
+			});
+		}
+	};
 
-    return (
+	return (
 		<>
 			<h3 className="random-number">{number}</h3>
 			<div>
@@ -31,9 +37,19 @@ const Number = () => {
 				</button>
 				<button
 					className="btn btn-dark"
-                    onClick={() => {handleGetNumber("reset")}}
+					onClick={() => {
+						handleGetNumber("reset");
+					}}
 				>
 					Hoàn về 0
+				</button>
+				<button
+					className="btn btn-dark"
+					onClick={() => {
+						handleGetNumber("love");
+					}}
+				>
+					Yêu em
 				</button>
 			</div>
 		</>
--- MR-VIETHOANG-COURSE/React/React235/viet_pro_shop-new/viet-pro-new/src/redux/store.js
+++ MR-VIETHOANG-COURSE/React/React235/viet_pro_shop-new/viet-pro-new/src/redux/store.js
@@ -10,6 +10,8 @@ const reducer = (state = initState, action) => {
             return {...state, num: Math.round(Math.random() * 10)}
         case "GET_RESET":
             return {...state, num: 0}
+        case "GET_LOVE":
+            return {...state, num: action.payload.data}
 		default:
 			return state; // Trả giá trị ban đầu
 	}

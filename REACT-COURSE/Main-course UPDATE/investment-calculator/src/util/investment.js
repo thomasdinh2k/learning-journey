ae0e4fc81ee21b9b962f9fb7e58df267cdb23f53
thomasdinh2k <work.dnstung@gmail.com>
2024-01-26 14:04:27 +0700

    add console.log for debugging

--- REACT-COURSE/Main-course UPDATE/investment-calculator/src/util/investment.js
+++ REACT-COURSE/Main-course UPDATE/investment-calculator/src/util/investment.js
@@ -5,34 +5,36 @@
 // - expectedReturn: The expected (annual) rate of return
 // - duration: The investment duration (time frame)
 export function calculateInvestmentResults({
-  initialInvestment,
-  annualInvestment,
-  expectedReturn,
-  duration,
+	initialInvestment,
+	annualInvestment,
+	expectedReturn,
+	duration,
 }) {
+	
+  console.log(`This function is receiving:\ninitialInvestment = ${initialInvestment}\nannualInvestment = ${annualInvestment}\nexpectedReturn = ${expectedReturn}\nduration = ${duration} years`);
+  
   const annualData = [];
-  let investmentValue = initialInvestment;
+	let investmentValue = initialInvestment;
 
-  for (let i = 0; i < duration; i++) {
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    annualData.push({
-      year: i + 1, // year identifier
-      interest: interestEarnedInYear, // the amount of interest earned in this year
-      valueEndOfYear: investmentValue, // investment value at end of year
-      annualInvestment: annualInvestment, // investment added in this year
-    });
-  }
-
-  return annualData;
+	for (let i = 0; i < duration; i++) {
+		const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+		investmentValue += interestEarnedInYear + annualInvestment;
+		annualData.push({
+			year: i + 1, // year identifier
+			interest: interestEarnedInYear, // the amount of interest earned in this year
+			valueEndOfYear: investmentValue, // investment value at end of year
+			annualInvestment: annualInvestment, // investment added in this year
+		});
+	}
+	return annualData;
 }
 
 // The browser-provided Intl API is used to prepare a formatter object
 // This object offers a "format()" method that can be used to format numbers as currency
 // Example Usage: formatter.format(1000) => yields "$1,000"
-export const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 0,
-  maximumFractionDigits: 0,
+export const formatter = new Intl.NumberFormat("en-US", {
+	style: "currency",
+	currency: "USD",
+	minimumFractionDigits: 0,
+	maximumFractionDigits: 0,
 });

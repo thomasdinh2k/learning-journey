6ae79ad49ca5f0d438e13f3a95c44c49c21e4efa
thomasdinh2k <work.dnstung@gmail.com>
2024-02-03 18:03:35 +0700

    export product_quantity to the view model

--- NODE-COURSE/viet-pro/src/apps/controllers/dashboardController.js
+++ NODE-COURSE/viet-pro/src/apps/controllers/dashboardController.js
@@ -1,9 +1,15 @@
-const dashboard = (req, res) => {
-	res.render("new_admin/dashboard");
+const productModel = require("../models/product");
+
+const dashboard = async (req, res) => {
+	const products = await productModel.find({});
+
+	console.log("Products = ", products.length);
+
+	res.render("new_admin/dashboard", {product_quantity: products.length});
 };
 
 const testAnnounce = (req, res) => {
-	res.send("Please route to <b> admin/dashboard <b> to go to the Dashboard")
-}
+	res.send("Please route to <b> admin/dashboard <b> to go to the Dashboard");
+};
 
-module.exports = {dashboard, testAnnounce};
+module.exports = { dashboard, testAnnounce };

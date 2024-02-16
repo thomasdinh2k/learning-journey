d81a3f4c75a0e2fad86351b3dc38c39324874ea5
thomasdinh2k <work.dnstung@gmail.com>
2024-02-16 16:14:41 +0700

    complete pagination logic

--- NODE-COURSE/viet-pro/src/apps/controllers/productController.js
+++ NODE-COURSE/viet-pro/src/apps/controllers/productController.js
@@ -1,11 +1,13 @@
-const { getProductData } = require("../../common/getProductData");
+const pagination = require("../../common/pagination")
+
+const { getProductData, getProductAmount } = require("../../common/getProductData");
 
 const productDisplay = async (req, res) => {
 	// Session
 	const userCredential = req.session.userCredential;
 
 	// Extract query parameter
-	const { page, limit, category } = req.query;
+	const { page, limit = 5, category } = req.query;
 
 	// Calculate Pagination
 	const pageNumber = parseInt(page, 10) || 1;
@@ -15,13 +17,21 @@ const productDisplay = async (req, res) => {
 	const test = { pageNumber, productPerPage, skip, category };
 	console.log("query test", test);
 
+	// Test new algorithm
+	const totalRow = await getProductAmount( {} );
+	const pageList = pagination(totalRow, limit, pageNumber);
+	console.log("Test New Algorithm Result", pageList);
+
+
 	// Query data
 	const data = await getProductData({ limit: limit, skip: skip });
-	console.log("Product_feed", data.length);
+	// console.log("Product_feed", data.length);
 
 	res.render("new_admin/Components/product/product_display", {
 		userCredential,
 		productData: data,
+		pageList,
+		currentPage: pageNumber
 	});
 };
 
---
+++ NODE-COURSE/viet-pro/src/common/pagination.js
@@ -0,0 +1,30 @@
+module.exports = (TotalRow, limit, currentPage) => {
+    // TODO: Tìm ra page đầu, page cuối, page hiện tại, 2 page trước hiện tại, 2 page sau hiện tại
+
+    // Page đầu = 0, page cuối = tổng số page, currentPage tự truyền xuống
+
+    const test_input = { TotalRow, limit, currentPage }
+    console.log("test_input", test_input);
+
+    const totalPageNum = Math.ceil( TotalRow / limit)
+    
+    let finalPageList = [];
+    const delta = 3;
+    let delta_right = currentPage + delta;
+    let delta_left = currentPage - delta;
+    // Chạy iteration, nếu số đạt điều kiện thì đưa vào trong Array
+    for (let i = 1; i <= totalPageNum; i++) {
+        if (
+            i == 1 || // First Page
+            i == currentPage ||
+            (i >= delta_left) && (i <= delta_right) ||
+            i == totalPageNum // Last Page
+        ) {
+            finalPageList.push(i);
+        }
+    }
+    let testPageListParam = {totalPageNum, finalPageList};
+
+    
+    return finalPageList;
+}

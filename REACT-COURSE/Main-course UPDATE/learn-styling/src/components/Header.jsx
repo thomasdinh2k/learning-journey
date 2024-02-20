9fc91d78324e4cad37feb29cd44197c7ef3e117b
thomasdinh2k <work.dnstung@gmail.com>
2024-02-20 17:49:42 +0700

    react-js--styling component using "styled-components"

--- REACT-COURSE/Main-course UPDATE/learn-styling/src/components/Header.jsx
+++ REACT-COURSE/Main-course UPDATE/learn-styling/src/components/Header.jsx
@@ -1,18 +1,18 @@
+/* eslint-disable no-unused-vars */
 import logo from "../assets/logo.png";
+import headerStyle from "../header.module.css";
 
 export default function Header() {
 	return (
 		<header>
 			<img src={logo} alt="A canvas" />
 			<h1>ReactArt</h1>
-			<p
-				style={{
-					color: "red",
-					fontWeight: "bolder",
-					textAlign: "center",
-				}}>
+			<p className={headerStyle.paragraph}>
 				A community of artists and art-lovers.
 			</p>
 		</header>
 	);
 }
+
+// This is a comment
+// TODO This is a TODO comment

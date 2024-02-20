5ea509349a5a608d50cb2abeefb30b1ecec72a01
thomasdinh2k <work.dnstung@gmail.com>
2024-02-20 15:27:32 +0700

    initialize new learning project

---
+++ REACT-COURSE/Main-course UPDATE/learn-styling/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.png";
+
+export default function Header() {
+	return (
+		<header>
+			<img src={logo} alt="A canvas" />
+			<h1>ReactArt</h1>
+			<p
+				style={{
+					color: "red",
+					fontWeight: "bolder",
+					textAlign: "center",
+				}}>
+				A community of artists and art-lovers.
+			</p>
+		</header>
+	);
+}

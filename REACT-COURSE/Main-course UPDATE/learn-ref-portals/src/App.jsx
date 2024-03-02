5da040ff71747e1179fd8fbebf5a99a0c8eed401
thomasdinh2k <work.dnstung@gmail.com>
2024-03-02 17:21:10 +0700

    additional commit

--- REACT-COURSE/Main-course UPDATE/learn-ref-portals/src/App.jsx
+++ REACT-COURSE/Main-course UPDATE/learn-ref-portals/src/App.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
+
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
-    <>
-      <Player />
-      <div id="challenges"></div>
-    </>
-  );
+		<>
+			<Player />
+			<div id="challenges">
+				<TimerChallenge title="Easy" targetTime={1} />
+				<TimerChallenge title="Tough" targetTime={10} />
+				<TimerChallenge title="Easy" targetTime={1} />
+				<TimerChallenge title="Easy" targetTime={1} />
+			</div>
+		</>
+	);
 }
 
 export default App;
--- REACT-COURSE/Main-course UPDATE/learn-ref-portals/src/components/Player.jsx
+++ REACT-COURSE/Main-course UPDATE/learn-ref-portals/src/components/Player.jsx
@@ -1,11 +1,28 @@
+import React from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+	const [ playerName, setPlayerName ] = React.useState();
+
+  const enteredPlayerName = React.useRef("");
+  
+	const changeName = () => {
+  
+    const name = enteredPlayerName.current.value;
+    
+    setPlayerName(name);
+
+    // Directly manipulate DOM value (not recommended)
+    enteredPlayerName.current.value = '';
+  
+  };
+
+	return (
+		<section id="player">
+			<h2>Welcome {playerName ?? "unknown entity"}</h2>
+			<p>
+				<input type="text" ref={enteredPlayerName}/>
+				<button onClick={changeName}>Set Name</button>
+			</p>
+		</section>
+	);
 }

9012e0ac5fc67cc34cbc59ad2e7d3c61a5250ef1
thomasdinh2k <work.dnstung@gmail.com>
2024-03-14 15:38:20 +0700

    Added TailwindCSS

--- REACT-COURSE/project-management-app/src/App.jsx
+++ REACT-COURSE/project-management-app/src/App.jsx
@@ -1,8 +1,11 @@
+import './index.css'
+
 function App() {
   return (
-    <>
+    <div className="bg-stone-50">
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+    </div>
   );
 }
 

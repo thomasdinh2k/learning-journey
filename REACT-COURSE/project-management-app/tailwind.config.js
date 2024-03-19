f24a05913e5307c40416abf47fdeda00a6acee1b
thomasdinh2k <work.dnstung@gmail.com>
2024-03-19 16:15:56 +0700

    intergrate with flowbite plugin

--- REACT-COURSE/project-management-app/tailwind.config.js
+++ REACT-COURSE/project-management-app/tailwind.config.js
@@ -1,9 +1,12 @@
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite-react/lib/esm/**/*.js",
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-});
+	plugins: [require("flowbite/plugin")],
+};

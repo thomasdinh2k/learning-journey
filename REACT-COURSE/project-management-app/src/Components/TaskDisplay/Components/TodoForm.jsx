c86fc1ee658629a1a3383b760b100aa86283b3a7
thomasdinh2k <work.dnstung@gmail.com>
2024-03-21 15:33:00 +0700

    fix: minor issues

--- REACT-COURSE/project-management-app/src/Components/TaskDisplay/Components/TodoForm.jsx
+++ REACT-COURSE/project-management-app/src/Components/TaskDisplay/Components/TodoForm.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 import Button from "../../SharedComponents/Button";
+import Modal from "../../SharedComponents/Modal";
 
 const TodoForm = ({handleCreateTodo, selectedProjectId}) => {
-	const [enteredTodo, setEnteredTodo] = useState(null);
+	const [enteredTodo, setEnteredTodo] = useState('');
 
 	const handleInputChange = (event) => {
 		setEnteredTodo(event.target.value);
 	};
 
 	const handleClick = () => {
-		handleCreateTodo( selectedProjectId, enteredTodo)
-		setEnteredTodo("");
+		
+		if (enteredTodo.trim() != '') {
+			handleCreateTodo( selectedProjectId, enteredTodo)
+			setEnteredTodo("");
+		} else {
+			<Modal  buttonLabel="Okay">
+			<h2 className="text-xl font-bold text-stone-700 my-4">
+				Dữ liệu nhập vào thiếu, hoặc không hợp lệ
+			</h2>
+			<p className="text-stone-600 mb-4">
+				Oops! Bạn nên check lại dữ liệu cần nhập
+			</p>
+			<p className="text-stone-600 mb-4">
+				Chắc chắn rằng bạn không để xót trường dữ liệu nào
+			</p>
+		</Modal>
+		}
 
 	};
 

5e0053654367d114d7898595f85ebe9b365243cc
thomasdinh2k <work.dnstung@gmail.com>
2024-04-04 16:55:46 +0700

    main-react--counter: Complete the project

--- REACT-COURSE/a-simple-counter/src/components/Counter/Counter.jsx
+++ REACT-COURSE/a-simple-counter/src/components/Counter/Counter.jsx
@@ -1,61 +1,86 @@
-import { useState } from 'react';
+import { useState, memo, useCallback, useMemo, useEffect } from "react"
 
-import IconButton from '../UI/IconButton.jsx';
-import MinusIcon from '../UI/Icons/MinusIcon.jsx';
-import PlusIcon from '../UI/Icons/PlusIcon.jsx';
-import CounterOutput from './CounterOutput.jsx';
-import { log } from '../../log.js';
+import IconButton from "../UI/IconButton.jsx"
+import MinusIcon from "../UI/Icons/MinusIcon.jsx"
+import PlusIcon from "../UI/Icons/PlusIcon.jsx"
+import CounterOutput from "./CounterOutput.jsx"
+import { log } from "../../log.js"
+import CounterHistory from "./CounterHistory.jsx"
 
 function isPrime(number) {
-  log(
-    'Calculating if is prime number',
-    2,
-    'other'
-  );
-  if (number <= 1) {
-    return false;
-  }
+	log("Calculating if is prime number", 2, "other")
 
-  const limit = Math.sqrt(number);
+	if (number <= 1) {
+		return false
+	}
 
-  for (let i = 2; i <= limit; i++) {
-    if (number % i === 0) {
-      return false;
-    }
-  }
+	const limit = Math.sqrt(number)
 
-  return true;
+	for (let i = 2; i <= limit; i++) {
+		if (number % i === 0) {
+			return false
+		}
+	}
+
+	return true
 }
 
-export default function Counter({ initialCount }) {
-  log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+const Counter = memo(function Counter({ initialCount, isFancy }) {
+	log("<Counter /> rendered", 1)
 
-  const [counter, setCounter] = useState(initialCount);
+	const initialCountIsPrime = useMemo(
+		() => isPrime(initialCount),
+		[initialCount]
+	)
 
-  function handleDecrement() {
-    setCounter((prevCounter) => prevCounter - 1);
-  }
+	useEffect(() => {
+		setCounterChanges([{ value: initialCount, id: Math.random() * 1000 }])
+	}, [initialCount])
 
-  function handleIncrement() {
-    setCounter((prevCounter) => prevCounter + 1);
-  }
+	// const [counter, setCounter] = useState(initialCount);
+	const [counterChanges, setCounterChanges] = useState([
+		{ value: initialCount, id: Math.random() * 1000 },
+	])
 
-  return (
-    <section className="counter">
-      <p className="counter-info">
-        The initial counter value was <strong>{initialCount}</strong>. It{' '}
-        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-      </p>
-      <p>
-        <IconButton icon={MinusIcon} onClick={handleDecrement}>
-          Decrement
-        </IconButton>
-        <CounterOutput value={counter} />
-        <IconButton icon={PlusIcon} onClick={handleIncrement}>
-          Increment
-        </IconButton>
-      </p>
-    </section>
-  );
-}
+	const currentCounter = counterChanges.reduce(
+		(prevCounter, counterChange) => prevCounter + counterChange.value,
+		0
+	)
+
+	const handleDecrement = useCallback(function handleDecrement() {
+		// setCounter((prevCounter) => prevCounter - 1);
+		setCounterChanges(prevCounterChanges => [
+			{ value: -1, id: Math.random() * 1000 },
+			...prevCounterChanges,
+		])
+	}, [])
+
+	const handleIncrement = useCallback(function handleIncrement() {
+		// setCounter((prevCounter) => prevCounter + 1);
+		setCounterChanges(prevCounterChanges => [
+			{ value: 1, id: Math.random() * 1000 },
+			...prevCounterChanges,
+		])
+	}, [])
+
+	return (
+		<section className="counter">
+			<p className="counter-info">
+				The initial counter value was <strong>{initialCount}</strong>. It{" "}
+				<strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
+			</p>
+			<p>
+				<IconButton icon={MinusIcon} onClick={handleDecrement}>
+					Decrement
+				</IconButton>
+				<CounterOutput value={currentCounter} />
+				<IconButton icon={PlusIcon} onClick={handleIncrement}>
+					Increment
+				</IconButton>
+			</p>
+			<CounterHistory history={counterChanges} />
+		</section>
+	)
+})
+
+export default Counter

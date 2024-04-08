a46534fd76a3fd13c7da266ee7ba6eb51c8a59e2
thomasdinh2k <work.dnstung@gmail.com>
2024-04-08 14:23:41 +0700

    main--react--practice-fetching-data: update user places through HTTP request

--- REACT-COURSE/pratice-fetching-data/src/App.jsx
+++ REACT-COURSE/pratice-fetching-data/src/App.jsx
@@ -1,85 +1,107 @@
-import { useRef, useState, useCallback } from 'react';
+import { useRef, useState, useCallback } from "react"
 
-import Places from './components/Places.jsx';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import AvailablePlaces from './components/AvailablePlaces.jsx';
-import { updateUserPlaces } from './http.js';
+import Places from "./components/Places.jsx"
+import Modal from "./components/Modal.jsx"
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx"
+import logoImg from "./assets/logo.png"
+import AvailablePlaces from "./components/AvailablePlaces.jsx"
+import { updateUserPlaces } from "./http.js"
+import ShowError from "./components/ShowError.jsx"
 
 function App() {
-  const selectedPlace = useRef();
+	const selectedPlace = useRef()
 
-  const [userPlaces, setUserPlaces] = useState([]);
+	const [userPlaces, setUserPlaces] = useState([])
 
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [modalIsOpen, setModalIsOpen] = useState(false)
 
-  function handleStartRemovePlace(place) {
-    setModalIsOpen(true);
-    selectedPlace.current = place;
-  }
+	const [ErrorUpdatingPlaces, setErrorUpdatingPlaces] = useState()
 
-  function handleStopRemovePlace() {
-    setModalIsOpen(false);
-  }
+	function handleStartRemovePlace(place) {
+		setModalIsOpen(true)
+		selectedPlace.current = place
+	}
 
-  // MARK: Handle Select Place
-  async function handleSelectPlace(selectedPlace) {
-    setUserPlaces((prevPickedPlaces) => {
-      if (!prevPickedPlaces) {
-        prevPickedPlaces = [];
-      }
-      if (prevPickedPlaces.some((place) => place.id === selectedPlace.id)) {
-        return prevPickedPlaces;
-      }
-      return [selectedPlace, ...prevPickedPlaces];
-    });
+	function handleStopRemovePlace() {
+		setModalIsOpen(false)
+	}
 
-    try {
-      await updateUserPlaces([selectedPlace, ...userPlaces]);
-      
-    } catch (error) {
-      console.log(error);
-    }
-  }
+	// MARK: Handle Select Place
+	async function handleSelectPlace(selectedPlace) {
+		setUserPlaces(prevPickedPlaces => {
+			if (!prevPickedPlaces) {
+				prevPickedPlaces = []
+			}
+			if (prevPickedPlaces.some(place => place.id === selectedPlace.id)) {
+				return prevPickedPlaces
+			}
+			return [selectedPlace, ...prevPickedPlaces]
+		})
 
-  const handleRemovePlace = useCallback(async function handleRemovePlace() {
-    setUserPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current.id)
-    );
+		try {
+			await updateUserPlaces([selectedPlace, ...userPlaces])
+		} catch (error) {
+			console.log(error)
+			setErrorUpdatingPlaces(
+				{ message: error.message } || "Failed to update places | Unknown error."
+			)
 
-    setModalIsOpen(false);
-  }, []);
+			// Fall back to the previous State
+			setUserPlaces(userPlaces)
+		}
+	}
 
-  return (
-    <>
-      <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
-        <DeleteConfirmation
-          onCancel={handleStopRemovePlace}
-          onConfirm={handleRemovePlace}
+	const handleRemovePlace = useCallback(async function handleRemovePlace() {
+		setUserPlaces(prevPickedPlaces =>
+			prevPickedPlaces.filter(place => place.id !== selectedPlace.current.id)
+		)
+
+		setModalIsOpen(false)
+	}, [])
+
+	const handleError = () => {
+		setErrorUpdatingPlaces(null)
+	}
+	return (
+		<>
+			<Modal open={ErrorUpdatingPlaces} onClose={handleError}>
+			
+        {ErrorUpdatingPlaces && (
+          <ShowError
+          title="Error while updating a Place"
+          message={ErrorUpdatingPlaces.message}
+          onConfirm={handleError}
         />
+        )}
       </Modal>
+      
+			<Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+				<DeleteConfirmation
+					onCancel={handleStopRemovePlace}
+					onConfirm={handleRemovePlace}
+				/>
+			</Modal>
 
-      <header>
-        <img src={logoImg} alt="Stylized globe" />
-        <h1>PlacePicker</h1>
-        <p>
-          Create your personal collection of places you would like to visit or
-          you have visited.
-        </p>
-      </header>
-      <main>
-        <Places
-          title="I'd like to visit ..."
-          fallbackText="Select the places you would like to visit below."
-          places={userPlaces}
-          onSelectPlace={handleStartRemovePlace}
-        />
+			<header>
+				<img src={logoImg} alt="Stylized globe" />
+				<h1>PlacePicker</h1>
+				<p>
+					Create your personal collection of places you would like to visit or
+					you have visited.
+				</p>
+			</header>
+			<main>
+				<Places
+					title="I'd like to visit ..."
+					fallbackText="Select the places you would like to visit below."
+					places={userPlaces}
+					onSelectPlace={handleStartRemovePlace}
+				/>
 
-        <AvailablePlaces onSelectPlace={handleSelectPlace} />
-      </main>
-    </>
-  );
+				<AvailablePlaces onSelectPlace={handleSelectPlace} />
+			</main>
+		</>
+	)
 }
 
-export default App;
+export default App

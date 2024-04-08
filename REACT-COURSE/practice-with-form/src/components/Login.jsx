25610784c9c5bc84ed4f378df8859eb2f3cbeb67
thomasdinh2k <work.dnstung@gmail.com>
2024-04-08 22:09:11 +0700

    main--react--practice-with-form: handle form submit via state

--- REACT-COURSE/practice-with-form/src/components/Login.jsx
+++ REACT-COURSE/practice-with-form/src/components/Login.jsx
@@ -1,24 +1,50 @@
+import { useState } from "react"
+
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+	const [inputValue, setInputValue] = useState({
+		email: "",
+		password: "",
+	})
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
+	const handleSubmission = (identifier, event) => {
+		setInputValue(prevState => ({
+			...prevState,
+			[identifier]: event.target.value,
+		}))
+	}
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
-      </div>
+  console.log("inputValue", inputValue);
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmission}>
+			<h2>Login</h2>
+
+			<div className="control-row">
+				<div className="control no-margin">
+					<label htmlFor="email">Email</label>
+					<input
+						id="email"
+						type="email"
+						name="email"
+						onChange={event => handleSubmission("email", event)}
+					/>
+				</div>
+
+				<div className="control no-margin">
+					<label htmlFor="password">Password</label>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						onChange={event => handleSubmission("password", event)}
+					/>
+				</div>
+			</div>
+
+			<p className="form-actions">
+				<button className="button button-flat">Reset</button>
+				<button className="button">Login</button>
+			</p>
+		</form>
+	)
 }
